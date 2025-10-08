f3efe5565d269a62118bbe066da51bf929dcc22a
ghostfreak10 <codewithghostfreak@gmail.com>
2025-10-08 19:12:52 +0530

    apiError fix

--- backend/src/utils/apiError.js
+++ backend/src/utils/apiError.js
@@ -4,7 +4,7 @@ class ApiError extends Error{
         this.message=message
         this.errors=errors
         this.statusCode=statusCode
-        this.success = success 
+        this.success = false 
 
         if (stack) {
             this.stack = stack
