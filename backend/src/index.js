c9777ad12ad8fc58e10c93e4319b6ca01e412b06
ghostfreak10 <codewithghostfreak@gmail.com>
2025-10-12 21:17:42 +0530

    chat app completed!

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,19 @@ import authRoutes from "./routes/auth.route.js"
 import app from './app.js'
 import connectDB from "./lib/db.js"
 import {server} from './lib/socket.js'
+import path from "path"
 
 dotenv.config({
     path: './.env'
 })
+const __dirname=path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 connectDB()
 .then(()=>{
     server.listen(process.env.PORT || 5000 , ()=>{
