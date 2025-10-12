ccce2377816c521e444deb1cfa1b001fba9fa7c8
ghostfreak10 <codewithghostfreak@gmail.com>
2025-10-12 22:18:47 +0530

    deployment error fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,9 +23,10 @@ app.use("/api/v1/users/messages",messageRoutes)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+  app.get("/:path*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
 }
 
 export default app
