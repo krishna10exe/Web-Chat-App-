ca0f93d66961020f4c2cf4e9d08ce5c92974b44e
ghostfreak10 <codewithghostfreak@gmail.com>
2025-10-12 23:11:55 +0530

    project may be completed!

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ import cors from "cors"
 import { app,server } from "./lib/socket.js"
 import path from "path"
 // const app = express();
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 app.use(cors({ origin:"http://localhost:5173", credentials: true }));
 app.use(express.json({ limit: "10mb" }));   
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -19,14 +19,16 @@ import messageRoutes from "./routes/message.route.js"
 // routes declarations
 app.use("/api/v1/users/auth",authRoutes)
 app.use("/api/v1/users/messages",messageRoutes)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   // SPA catch-all route for Express 5
-  app.get("/:path*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+ // SPA catch-all route
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+// });
+
+// }
 
 
 export default app
