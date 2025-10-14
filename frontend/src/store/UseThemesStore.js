887be8a76b5c049f1a4ff4e8edaa96a30c7192cd
ghostfreak10 <codewithghostfreak@gmail.com>
2025-10-14 20:51:29 +0530

    default theme changed

--- frontend/src/store/UseThemesStore.js
+++ frontend/src/store/UseThemesStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { THEMES } from "../constants";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("chat-theme") || "coffee",
+  theme: localStorage.getItem("chat-theme") || "Black",
   setTheme: (theme) => {
     localStorage.setItem("chat-theme", theme);
     set({ theme });
