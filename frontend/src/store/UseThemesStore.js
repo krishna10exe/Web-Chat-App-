dc69a82f68cfecf4a98af5c4ced6bc7bd4808ec8
ghostfreak10 <codewithghostfreak@gmail.com>
2025-10-14 20:58:55 +0530

    project may be completed!

--- frontend/src/store/UseThemesStore.js
+++ frontend/src/store/UseThemesStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { THEMES } from "../constants";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("chat-theme") || "Black",
+  theme: localStorage.getItem("chat-theme") || "black",
   setTheme: (theme) => {
     localStorage.setItem("chat-theme", theme);
     set({ theme });
